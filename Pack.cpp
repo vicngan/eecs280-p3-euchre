7b29525829d79a8a3618e3bc5e8139207841f6c3
victoria nguyen <vicngan@umich.edu>
2026-02-14 12:29:48 -0500

    initializing pack

--- Pack.cpp
+++ Pack.cpp
@@ -7,7 +7,21 @@ Pack::Pack(){
     for (int s = SPADES; s <= DIAMONDS; ++s){
         //inner loop lowest -> highest rank (2-> Ace)
         for (int r = TWO; r <= ACE; ++r){
+            //cast int to enum types 
             Suit current_suit = static_cast<Suit>(s); 
+            Rank current_rank = static_cast<Rank>(r);
+            //create card and add to pack 
+            cards[index] = Card(current_rank, current_suit);
+            index++; 
+        }
+    }
+}
+
+Pack::Pack(std::istream& pack_input){
+    int index = 0;
+    for (int i = 0; i < PACK_SIZE; ++i){
+        pack_input >> cards[index];
+        index++; 
     }
 }
 
